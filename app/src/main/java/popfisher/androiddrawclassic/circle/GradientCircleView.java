0431043b0858c0d361fe4321369f80dca49333f2
popfisher <774232122@qq.com>
2016-12-07 19:15:37 +0800

    添加渐变实现shape,边框shape

--- app/src/main/java/popfisher/androiddrawclassic/circle/GradientCircleView.java
+++ app/src/main/java/popfisher/androiddrawclassic/circle/GradientCircleView.java
@@ -1,11 +1,13 @@
-package popfisher.androiddrawclassic;
+package popfisher.androiddrawclassic.circle;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
+import android.graphics.RadialGradient;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
@@ -18,17 +20,20 @@ import android.view.View;
  * 绘制带渐变填充颜色的椭圆（园）
  */
 
-public class GradientCircle extends View {
+public class GradientCircleView extends View {
 
     private Paint mPaintGradientCircle;
     private RectF mRectF;
     private LinearGradient mLinearGradient;
+    private RadialGradient mRadialGradient;
+
     private static final int mStrokeColor = 0x80e4e4e4;
 
     private int mFillColors[] = new int[]{ 0x73ffffff, 0x5effffff, 0x5effffff, 0x80ffffff, 0x99ffffff, 0xe6ffffff };
+    private int mFillColors2[] = new int[]{ 0xff000000, 0x10000000,0x0c000000};
 
 
-    public GradientCircle(Context context, AttributeSet attrs) {
+    public GradientCircleView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
@@ -49,18 +54,24 @@ public class GradientCircle extends View {
      */
     private void drawDirect(Canvas canvas) {
         mPaintGradientCircle.setStyle(Paint.Style.STROKE);
-        mPaintGradientCircle.setStrokeWidth(1);
-        mPaintGradientCircle.setColor(mStrokeColor);
         mPaintGradientCircle.setShader(null);
-        canvas.drawOval(mRectF, mPaintGradientCircle);
-        // 画圆可以用下面的方法
-        // canvas.drawCircle(mRectF.centerX(), mRectF.centerY(), mRectF.width() / 2, mPaintGradientCircle);
 
+        mPaintGradientCircle.setStrokeWidth(7);
+        mPaintGradientCircle.setShader(mRadialGradient);
+        canvas.drawCircle(mRectF.centerX(), mRectF.centerY() + 3, mRectF.width() / 2 + 3, mPaintGradientCircle);
+
+
+        mPaintGradientCircle.setColor(mStrokeColor);
+        mPaintGradientCircle.setStrokeWidth(2);
+        mPaintGradientCircle.setShader(null);
+        canvas.drawCircle(mRectF.centerX(), mRectF.centerY(), mRectF.width() / 2, mPaintGradientCircle);
+
+        mPaintGradientCircle.setColor(mStrokeColor);
         mPaintGradientCircle.setStyle(Paint.Style.FILL);
         mPaintGradientCircle.setShader(mLinearGradient);
-        canvas.drawOval(mRectF, mPaintGradientCircle);
+
         // 画圆可以用下面的方法
-        // canvas.drawCircle(mRectF.centerX(), mRectF.centerY(), mRectF.width() / 2, mPaintGradientCircle);
+        canvas.drawCircle(mRectF.centerX(), mRectF.centerY(), mRectF.width() / 2, mPaintGradientCircle);
     }
 
     /**
@@ -77,6 +88,7 @@ public class GradientCircle extends View {
         final int padding = 50;
         mRectF = new RectF(padding, padding, getWidth() - padding, getHeight() - padding);
         mLinearGradient = new LinearGradient(mRectF.centerX(), mRectF.top, mRectF.centerX(), mRectF.bottom, mFillColors, null, Shader.TileMode.MIRROR);
+        mRadialGradient = new RadialGradient(mRectF.centerX(), mRectF.centerY(), mRectF.width() / 2 + 6, mFillColors2, null, Shader.TileMode.MIRROR);
     }
 
     private Bitmap drawableToBitamp(Drawable drawable, int w, int h) {
@@ -95,6 +107,10 @@ public class GradientCircle extends View {
         return drawableToBitamp(createGradientDrawable(), buttonBmpWidth, buttonBmpHeight);
     }
 
+    /**
+     * 生成的Drawable可以直接设置为View的背景
+     * @return
+     */
     private GradientDrawable createGradientDrawable() {
         GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, mFillColors);
         gradientDrawable.setGradientType(GradientDrawable.LINEAR_GRADIENT);
