35b16822a8b3181e65679d476ee463e1305fd1cd
popfisher <774232122@qq.com>
2016-12-28 22:28:24 +0800

    增加LevelListDrawable，level-list

--- app/src/main/java/popfisher/androiddrawclassic/XmlDrawableActivity.java
+++ app/src/main/java/popfisher/androiddrawclassic/XmlDrawableActivity.java
@@ -2,12 +2,26 @@ package popfisher.androiddrawclassic;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class XmlDrawableActivity extends Activity {
+public class XmlDrawableActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sys_drawable_xml);
+        initViews();
+    }
+
+    private void initViews() {
+        findViewById(R.id.drawable_selector_1).setOnClickListener(this);
+        findViewById(R.id.drawable_selector_2).setOnClickListener(this);
+        ((ImageView) findViewById(R.id.drawable_level_1)).setImageLevel(5);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
