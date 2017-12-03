8937b642f73a036d9b216185d26eb67b2108fa2d
popfisher <774232122@qq.com>
2017-12-03 22:04:15 +0800

    完善框架

--- app/src/main/java/popfisher/androiddrawclassic/canvasdraw/BasicDrawActivity.java
+++ app/src/main/java/popfisher/androiddrawclassic/canvasdraw/BasicDrawActivity.java
@@ -6,8 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 
 import popfisher.androiddrawclassic.R;
+import popfisher.androiddrawclassic.canvasdraw.bitmap.DrawBitmapActivity;
+import popfisher.androiddrawclassic.canvasdraw.circle.DrawCircleActivity;
+import popfisher.androiddrawclassic.canvasdraw.gradient.DrawGradientActivity;
 import popfisher.androiddrawclassic.canvasdraw.line.DrawLineActivity;
+import popfisher.androiddrawclassic.canvasdraw.oval.DrawOvalActivity;
 import popfisher.androiddrawclassic.canvasdraw.path.DrawPathActivity;
+import popfisher.androiddrawclassic.canvasdraw.picture.DrawPictureActivity;
+import popfisher.androiddrawclassic.canvasdraw.rect.DrawRectActivity;
+import popfisher.androiddrawclassic.canvasdraw.roundrect.DrawRoundRectActivity;
 import popfisher.androiddrawclassic.canvasdraw.text.DrawTextActivity;
 import popfisher.androiddrawclassic.canvasdraw.triangle.DrawTriangleActivity;
 import popfisher.androiddrawclassic.samples.sample1.Sample1Activity;
@@ -26,6 +33,13 @@ public class BasicDrawActivity extends Activity implements View.OnClickListener 
         findViewById(R.id.basic_draw_path).setOnClickListener(this);
         findViewById(R.id.basic_draw_triangle).setOnClickListener(this);
         findViewById(R.id.basic_draw_text).setOnClickListener(this);
+        findViewById(R.id.basic_draw_bitmap).setOnClickListener(this);
+        findViewById(R.id.basic_draw_circle).setOnClickListener(this);
+        findViewById(R.id.basic_draw_gradient).setOnClickListener(this);
+        findViewById(R.id.basic_draw_oval).setOnClickListener(this);
+        findViewById(R.id.basic_draw_picture).setOnClickListener(this);
+        findViewById(R.id.basic_draw_rect).setOnClickListener(this);
+        findViewById(R.id.basic_draw_rount_rect).setOnClickListener(this);
     }
 
     @Override
@@ -43,6 +57,27 @@ public class BasicDrawActivity extends Activity implements View.OnClickListener 
                 break;
             case R.id.basic_draw_triangle:
                 jumpToActivity(DrawTriangleActivity.class);
+                break;
+            case R.id.basic_draw_bitmap:
+                jumpToActivity(DrawBitmapActivity.class);
+                break;
+            case R.id.basic_draw_circle:
+                jumpToActivity(DrawCircleActivity.class);
+                break;
+            case R.id.basic_draw_gradient:
+                jumpToActivity(DrawGradientActivity.class);
+                break;
+            case R.id.basic_draw_oval:
+                jumpToActivity(DrawOvalActivity.class);
+                break;
+            case R.id.basic_draw_picture:
+                jumpToActivity(DrawPictureActivity.class);
+                break;
+            case R.id.basic_draw_rect:
+                jumpToActivity(DrawRectActivity.class);
+                break;
+            case R.id.basic_draw_rount_rect:
+                jumpToActivity(DrawRoundRectActivity.class);
                 break;
         }
     }
